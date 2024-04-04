8f0a5355e5c3b68e6e1d942388e24ff95085ce6b
Adi Israel <adi.israel@netapp.com>
2024-04-05 00:59:30 +0300

    changes for testing pipeline

--- ecr.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_ecr_repository" "web_server_repo" {
-  name                 = "web-server-repo"
-  image_tag_mutability = "MUTABLE"
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
