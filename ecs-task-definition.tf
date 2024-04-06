b2ec9d253e6e066f4c9c17bcfc95cf4ac59f74e6
Adi Israel <adi.israel@netapp.com>
2024-04-06 13:27:01 +0300

    testing artifacts + adding task to use var

--- ecs-task-definition.tf
+++ ecs-task-definition.tf
@@ -9,7 +9,7 @@ resource "aws_ecs_task_definition" "web_server_task" {
   container_definitions = jsonencode([
     {
       name      = "web-server",
-      image     = "619312328382.dkr.ecr.us-east-1.amazonaws.com/sportbet-web-server:latest",
+      image     = "619312328382.dkr.ecr.us-east-1.amazonaws.com/sportbet-web-server:${var.docker_image_tag}",
       cpu       = 128,
       memory    = 256,
       essential = true,
@@ -23,4 +23,7 @@ resource "aws_ecs_task_definition" "web_server_task" {
   ])
 }
 
-
+variable "docker_image_tag" {
+  description = "The tag of the Docker image to deploy"
+  type        = string
+}
